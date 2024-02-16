2255bbe641287e360457c0c8dbdb55f9d76df383
bendo <abdoelbendary8@gmail.com>
2024-02-16 22:52:11 +0200

    Initial App

---
+++ lib/screens/intro.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+  static String routeName = "/introScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.green,
+    );
+  }
+}
